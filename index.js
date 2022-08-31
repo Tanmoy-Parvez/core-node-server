b7f8a29f938d2ca5091502f51ec081de83e0cacb
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-09-01 00:27:40 +0600

    create a html response for browser

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const server = http.createServer(function (req, res) {
     const url = req.url;
     if (url == "/") {
         res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({greetings: "Hello node js"}))
+        res.end(JSON.stringify({ greetings: "Hello node js" }))
+    }
+    else if (url == "/home") { 
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<p>Hello Coders</p>");
+        res.end()
     }
 });
 
