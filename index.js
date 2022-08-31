bd474a384cb837b2cb04435f0fbcde9592c51289
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-09-01 00:58:19 +0600

    exploring url modules

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const http = require('http');
+const url = require('url');
 
-const server = http.createServer(function (req, res) {
+//normal server configuration
+
+/* const server = http.createServer(function (req, res) {
     const url = req.url;
     if (url == "/") {
         res.writeHead(200, { "Content-Type": "application/json" });
@@ -11,7 +14,15 @@ const server = http.createServer(function (req, res) {
         res.write("<p>Hello Coders</p>");
         res.end()
     }
+}); */
+
+const server = http.createServer(function (req, res) {
+    const address_url = "http://localhost:5000/user?name=tanmoy&country=BD";
+    const parsed_url = url.parse(address_url);
+    const queryObj = parsed_url.query
+    console.log(queryObj);
 });
+
 
 const port = 5000;
 
